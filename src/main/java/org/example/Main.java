7c99e2ac61c3d74ef2bfacc546c434b095cd5ab5
Andreas Pihlalkjær <andreaspihlalkjaer+gh@gmail.com>
2026-01-16 14:06:43 +0100

    Final GitHub attempt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,8 +25,8 @@ public class Main {
         // new reader
         Scanner reader = new Scanner(System.in);
         //accountChoice
-        int accountChoice;
-
+        int accountChoice= 0;
+        boolean validInput = false;
         Statement stmt = connection.createStatement();
         ResultSet resultSet = stmt.executeQuery("SELECT kundeID,name FROM kunde");
         while(resultSet.next()){
@@ -34,48 +34,24 @@ public class Main {
             String name = resultSet.getString("name");
             System.out.println("ID "+(id+1)+" name "+ name);
         }
-        System.out.println("choose account (1-4): ");
-        accountChoice = reader.nextInt() - 1;
+
+        while (validInput == false){
+            System.out.println("choose account (1-4): ");
+            accountChoice= reader.nextInt();
+            if(1<=accountChoice&&accountChoice<=4){
+                accountChoice = accountChoice - 1;
+                validInput = true;
+            } else {
+                System.out.println("Please enter a correct ID");
+
+            }
+        }
+
+
 
         return accountChoice;
+
     }
 
-    static void showDB() throws SQLException {
-        try {
-            //
-            connectDB db = new connectDB();
-            Connection connection = db.getLocalConnection();
 
-            Statement stmt = connection.createStatement();
-            ResultSet resultSet = stmt.executeQuery("SELECT * FROM kunde");
-
-            //fetches metadata from tables, amount of columns, column names.
-            ResultSetMetaData meta = resultSet.getMetaData();
-            int columnCount = meta.getColumnCount();
-
-            //prints column data for how many columns there are.
-            while (resultSet.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(resultSet.getString(i) + "\t");
-                }
-                System.out.println();
-
-                // Print column headers
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(meta.getColumnName(i) + "\t");
-                }
-                System.out.println();
-
-                // Print rows
-                while (resultSet.next()) {
-                    for (int i = 1; i <= columnCount; i++) {
-                        System.out.print(resultSet.getString(i) + "\t");
-                    }
-                    System.out.println();
-                }
-            }
-        }catch(RuntimeException e){
-            throw new RuntimeException(e);
-        }
     }
-}
