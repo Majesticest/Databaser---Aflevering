0c741ae10329bd9a3c6cfa5b81e3ac8bb8fd0fa3
Rogultran <maksymplakhotnyuk2@gmail.com>
2026-01-20 18:35:17 +0100

    Added productBrowser.java Where OG productBrowser method got moved to and renamed to displayProducts(). probably should also move SelectUser to its own class aswell

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,8 @@ public class Main {
             try {
                 // Select which account the user would like to continue with.
                 int accountChoice = selectUser();
-                //
-                productBrowser();
+                //Displays all products regardless of availability
+                productBrowser.displayProducts();
 
             } catch (SQLException e) {
                 throw new RuntimeException(e);
@@ -39,34 +39,5 @@ public class Main {
         accountChoice = reader.nextInt() - 1;
 
         return accountChoice;
-    }
-    //displays all products
-    private static void productBrowser() throws SQLException {
-        // Method setup to connectDB and database connection
-        connectDB db = new connectDB();
-        Connection connection = db.getLocalConnection();
-        // new reader for method
-        Scanner reader = new Scanner(System.in);
-
-        Statement stmt = connection.createStatement();
-        ResultSet resultSet = stmt.executeQuery("SELECT productID,price,name,amount FROM product");
-
-        //Display products and stock availability
-        while(resultSet.next()){
-            int lowSupply = 100;
-            int id = resultSet.getInt("productID");
-            int price = resultSet.getInt("price");
-            String name = resultSet.getString("name");
-            int amount = resultSet.getInt("amount");
-
-            if(amount > lowSupply){
-                System.out.println("nr "+id+" product: "+name+" | "+ price+" kr");
-            } else if (amount == 0) {
-                System.out.println("nr "+id+" product: "+name+" | "+ price+" kr"+" Sold out");
-            } else{
-                System.out.println("nr "+id+" product: "+name+" | "+ price+" kr"+" Low Stock");
-            }
-
-        }
     }
 }
