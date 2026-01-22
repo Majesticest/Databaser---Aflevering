a9c1f82ff0eecaaace13e9559f66cc12073fb680
Andreas Pihlalkjær <andreaspihlalkjaer+gh@gmail.com>
2026-01-22 13:57:34 +0100

    Final GitHub attempt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,14 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
+    public static int accountChoice;
     public static void main(String[] args) {
         connectDB db = new connectDB();
         Connection connection = db.getLocalConnection();
         if (connection != null) {
             try {
                 // Select which account the user would like to continue with.
-                int accountChoice = selectUser();
+
                 //Displays all products regardless of availability
                 productBrowser.displayProducts();
 
