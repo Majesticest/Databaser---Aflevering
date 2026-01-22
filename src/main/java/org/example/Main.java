eff35eebfc1d2ea71ad0e865fe908c7eda03fdef
Rogultran <maksymplakhotnyuk2@gmail.com>
2026-01-22 13:31:00 +0100

    Added categoryEnum.java category table in entityDB is now redundant. Added productSelect in productBrowser.java.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -35,7 +35,7 @@ public class Main {
             System.out.println("ID " + (id + 1) + " name " + name);
         }
 
-        while (validInput == false) {
+        while (!validInput) {
             System.out.println("choose account (1-4): ");
             accountChoice = reader.nextInt();
             if (1 <= accountChoice && accountChoice <= 4) {
--- src/main/java/org/example/productBrowser.java
+++ src/main/java/org/example/productBrowser.java
@@ -1,38 +1,80 @@
 package org.example;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class productBrowser {
+
+
     //displays all products
     public static void displayProducts() throws SQLException{
         connectDB db = new connectDB();
         Connection connection = db.getLocalConnection();
-        // new reader for method
         Scanner reader = new Scanner(System.in);
+        //laver et arraylist for de forskellige ID'er som der findes i systemet.
+        ArrayList<Integer> ProductIDList = new ArrayList<>();
+
 
         Statement stmt = connection.createStatement();
         ResultSet resultSet = stmt.executeQuery("SELECT * FROM product");
 
         //Display products and stock availability
+        int id;
+        int price;
+        String name;
+        int amount;
         while(resultSet.next()){
             int lowSupply = 100;
-            int id = resultSet.getInt("productID");
-            int price = resultSet.getInt("price");
-            String name = resultSet.getString("name");
-            int amount = resultSet.getInt("amount");
+            id = resultSet.getInt("productID");
+            ProductIDList.add(id);
+            price = resultSet.getInt("price");
+            name = resultSet.getString("name");
+            amount = resultSet.getInt("amount");
+            int categoryID = resultSet.getInt("categoryID");
+
         // stock availability display
             if(amount > lowSupply){
-                System.out.println("nr "+id+" product: "+name+" | "+ price+" kr");
+                System.out.println("nr "+ id +" product: "+ name +" | "+ price +" kr");
             } else if (amount == 0) {
-                System.out.println("nr "+id+" product: "+name+" | "+ price+" kr"+" Sold out");
+                System.out.println("nr "+ id +" product: "+ name +" | "+ price +" kr"+" Sold out");
             } else{
-                System.out.println("nr "+id+" product: "+name+" | "+ price+" kr"+" Low Stock");
+                System.out.println("nr "+ id +" product: "+ name +" | "+ price +" kr"+" Low Stock");
             }
 
         }
+
+        int productSelection = reader.nextInt();
+        if(productSelection >= ProductIDList.getFirst() && productSelection <= ProductIDList.getLast() ){
+            ResultSet resultSetLocal = stmt.executeQuery("SELECT * FROM product WHERE productID='"+productSelection+"'");
+            //Henter det specifikke data
+            id = resultSet.getInt("productID");
+            price = resultSet.getInt("price");
+            name = resultSet.getString("name");
+            amount = resultSet.getInt("amount");
+            int categoryID = resultSet.getInt("categoryID");
+            String CatID = EnumTranslator(categoryID);
+
+            //printer dataet
+            System.out.println("nr " + id +" | Product: "+ name);
+            System.out.println("price: "+ price + "kr | amount: "+ amount + " | "+CatID);
+        }
+
         resultSet.close();
         stmt.close();
         connection.close();
+    }
+
+    //translates the enums to actual names
+    public static String EnumTranslator(int categoryID){
+        String CategoryType = null;
+        switch(categoryID){
+            case 0-> CategoryType = String.valueOf(category.sko);
+            case 1-> CategoryType = String.valueOf(category.bukser);
+            case 2-> CategoryType = String.valueOf(category.bluser);
+            case 3-> CategoryType = String.valueOf(category.hatte);
+            case 4-> CategoryType = String.valueOf(category.sokker);
+        }
+        return CategoryType;
     }
 }
 
